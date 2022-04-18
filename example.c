5da4461d40353e92e9c358e0db57fbba86e66c26
vulcan-action <vulcan@action>
2022-04-18 02:24:04 +0000

    initial commit

---
+++ example.c
@@ -0,0 +1,17 @@
+int mid(int x, int y, int z){
+    int ret = z;
+    if(y < z) {
+        if(x < y){
+            ret = y;
+        }else if(x < z){
+            ret = y;
+        }
+    } else {
+        if(x > y){
+            ret = y;
+        }else if(x > z){
+
+        }
+    }
+    return ret;
+}
