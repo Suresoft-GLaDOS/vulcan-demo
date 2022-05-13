70ed20bb84706defcbccf7471482c1496f964b14
vulcan-action <vulcan@action>
2022-05-13 07:15:42 +0000

    Committed the automatically generated patch

--- example.c
+++ example.c
@@ -19,7 +19,8 @@ int get_min(int n) {
 		ll temp = LLONG_MAX;
 		for (int j = 2; j <= 7; j++) {
             int temp2 = dp[i - j] * 10 + num[j];
-            temp = min(temp, temp2);
+          dp[5] = (3);
+temp = min(temp, temp2);
         }
 		dp[i] = temp;
 	}
